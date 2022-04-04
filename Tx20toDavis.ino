e97b1891ce256056e0b4bf87497e08592a172a59
Plop3 <serge@plop3.fr>
2022-04-04 16:47:52 +0200

    Create Tx20toDavis.ino

---
+++ Tx20toDavis.ino
@@ -0,0 +1,19 @@
+/*
+Lacrosse anemometer TX20 protocol converter to Davis anemometer protocol
+Use an ESP32 (ADC 0-3.3V output)
+Serge CLAUS
+Version 0.1
+GPL V3
+
+Inputs:
+  TX20 data
+Outputs:
+  OUT0: Wind speed (frequency)
+  ADC0: Wind direction
+*/
+
+void setup() {
+}
+
+void loop() {
+}
